b972d74a8890f783734a6cf99c4d59cc729b0b5d
Nestor Vera <nestorvera@me.com>
2019-10-14 15:41:59 +0200

    use new plugins API to fix defaults not being overwritable

--- index.js
+++ index.js
@@ -1,13 +1,16 @@
-const tailwindConfig = require('./tailwind.config.js')
 const { buildPlugin } = require('@hacknug/tailwindcss-plugin-utils')
+const defaultConfig = require('./tailwind.config.js')
 
-module.exports = function (pluginConfig) {
-  return function (coreUtils) {
-    return buildPlugin(coreUtils, tailwindConfig, [
-      { key: ['textFillColor', 'borderColor'], base: 'text-fill', property: '-webkit-text-fill-color' },
-      { key: ['textStrokeColor', 'borderColor'], base: 'text-stroke', property: '-webkit-text-stroke-color' },
-      { key: ['textStrokeWidth', 'borderWidth'], base: 'text-stroke', property: '-webkit-text-stroke-width' },
-      { key: ['paintOrder'], base: 'paint' },
-    ])
+module.exports = function (pluginOptions) {
+  return {
+    config: defaultConfig,
+    handler (coreUtils) {
+      return buildPlugin(coreUtils, [
+        { key: ['textFillColor', 'borderColor'], base: 'text-fill', property: '-webkit-text-fill-color' },
+        { key: ['textStrokeColor', 'borderColor'], base: 'text-stroke', property: '-webkit-text-stroke-color' },
+        { key: ['textStrokeWidth', 'borderWidth'], base: 'text-stroke', property: '-webkit-text-stroke-width' },
+        { key: ['paintOrder'], base: 'paint' },
+      ])
+    },
   }
 }
